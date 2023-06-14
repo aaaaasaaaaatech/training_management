e1423fa5454e50601117e55e54d9b59293f31e30
aaaaasaaaaatech <132049862+aaaaasaaaaatech@users.noreply.github.com>
2023-06-14 18:03:21 +0900

    バリデーション､投稿機能実装

--- app/models/boxing.rb
+++ app/models/boxing.rb
@@ -3,6 +3,7 @@ class Boxing < ActiveHash::Base
     { id: 1, name: '実施内容' },
     { id: 2, name: '初級コンビネーション' },
     { id: 3, name: '上級コンビネーション' },
+    { id: 4, name: '上級コンビネーション2' }
   ]
 
   include ActiveHash::Associations
--- app/models/training.rb
+++ app/models/training.rb
@@ -1,4 +1,6 @@
 class Training < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :training
+  belongs_to :boxing
+
+  validates :boxing_id, numericality: { other_than: 1 , message: "必ず選んでください"}
 end
