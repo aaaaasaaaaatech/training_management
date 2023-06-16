a79519d3e4624ba01cb01497253b8aa5e4802bc0
aaaaasaaaaatech <132049862+aaaaasaaaaatech@users.noreply.github.com>
2023-06-16 12:35:03 +0900

    編集､更新､削除

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -6,18 +6,37 @@ class TrainingsController < ApplicationController
   end
 
   def new
-    @trainings = Training.new
+    @training = Training.new
   end
 
   def create
-    @trainings = Training.new(training_params)
-    if @trainings.save
-      
+    @training = Training.new(training_params)
+    if @training.save
       redirect_to root_path
     else
       flash[:notice] = "必ず選んでください"
       render :new
     end
+  end
+
+  def edit
+     @training = Training.find(params[:id])
+  end
+
+  def update
+    if training = Training.find(params[:id])
+      training.update(training_params)
+      redirect_to root_path
+    else
+      flash[:notice] = "必ず選んでください"
+      render :edit
+    end
+  end
+
+  def destroy
+    training = Training.find(params[:id])
+    training.destroy
+    redirect_to action: :index
   end
 
   private
